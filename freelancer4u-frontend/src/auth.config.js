9074ef06a8595dd5eacb26ca2919e4071b518d72
blobb3 <janine.heinemeyer@gmail.com>
2024-02-04 15:44:53 +0100

    nachtraegliches

--- freelancer4u-frontend/src/auth.config.js
+++ freelancer4u-frontend/src/auth.config.js
@@ -1,6 +1,6 @@
 const config = {
-    domain: "...",
-    clientId: "..."
+    domain: "dev-xx5vk871bealw7qc.eu.auth0.com",
+    clientId: "qQHm0Y3JfGAVXqUbUMDBgmzSGwRbET2O"
 };
   
 export default config;
